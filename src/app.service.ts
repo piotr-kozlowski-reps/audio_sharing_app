ea6f3234d236c86dac891fcba55e51e4bfe0074e
Piotr Kozlowski <pegaz@ante.pl>
2024-02-05 09:00:23 +0100

    init

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
+  getHello(): string {
+    this.logger.log('Hello World! -> hit');
+    return 'Hello World!';
+  }
+}
